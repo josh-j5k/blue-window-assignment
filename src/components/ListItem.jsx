4df003b913fc0a1c3c5795032c0b0b25f0f2b4f3
josh-j5k <joshdejong5k@gmail.com>
2024-07-22 12:18:20 +0100

    development

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -6,7 +6,7 @@ import StarEmpty from "./StarEmpty"
 export default function ListItem({ item }) {
 	return (
 		<div role="listitem" className="w-clamp shadow-round mx-auto p-4">
-			<div className="grid desktop-min:grid-cols-4 -lg:grid-cols-2 -md:grid-cols-1 desktop-min:gap-8  gap-4">
+			<div className="grid desktop-min:grid-cols-[25%_20%_20%_25%] -lg:grid-cols-2 -md:grid-cols-1 desktop-min:gap-8  gap-4">
 				{/* image container */}
 				<div
 					className="-lg:row-start-1 -lg:row-end-3 -md:row-start-1 -md:row-end-2 -lg:col-start-1 -lg:col-end-2 flex justify-center"
@@ -16,90 +16,92 @@ export default function ListItem({ item }) {
 				</div>
 				{/* stars and casino name container */}
 				<div className="flex -lg:row-start-1 -lg:row-end-2 -lg:col-start-2 -lg:col-end-3 -md:col-start-1 -md:col-end-2 -md:row-start-2 -md:row-end-3 flex-col gap-2 desktop-min:justify-center desktop-min:items-center -md:justify-center -md:items-center">
-					<span className="capitalize underline pl-2">{item.casino}</span>
-					{/* stars filled */}
-					<span
-						title="rating"
-						aria-describedby={item.reviews.desktop + " star rating"}
-						className="desktop-min:flex hidden"
-					>
+					<div className="-md:flex flex-col gap-2 -md:justify-center">
+						<span className="capitalize underline">{item.casino}</span>
+						{/* stars filled */}
 						<span
+							title="rating"
 							aria-describedby={item.reviews.desktop + " star rating"}
-							className="flex"
+							className="desktop-min:flex hidden"
 						>
-							{Array.from(Array(item.reviews.desktop)).map((_, index) => (
-								<span key={index}>
-									<StarFilled width="31" height="31" />
-								</span>
-							))}
-						</span>
-						<span className="flex">
-							{Array.from(Array(5 - item.reviews.desktop)).map((_, index) => (
-								<span key={index}>
-									<StarEmpty width="31" height="31" />
-								</span>
-							))}
-						</span>
-					</span>
-					<span
-						aria-describedby={item.reviews.tablet + " star rating"}
-						className="-lg:flex -md:hidden hidden"
-					>
-						<span className="flex">
-							{Array.from(Array(item.reviews.tablet)).map((_, index) => (
-								<span key={index}>
-									<StarFilled width="31" height="31" />
-								</span>
-							))}
-						</span>
-						<span className="flex">
-							{Array.from(Array(5 - item.reviews.tablet)).map((_, index) => (
-								<span key={index}>
-									<StarEmpty width="31" height="31" />
-								</span>
-							))}
-						</span>
-					</span>
-					<span
-						aria-describedby={item.reviews.mobile + " star rating"}
-						className="-md:flex -sm:hidden hidden"
-					>
-						<span className="flex">
-							{Array.from(Array(item.reviews.mobile)).map((_, index) => (
-								<span key={index}>
-									<StarFilled width="31" height="31" />
-								</span>
-							))}
-						</span>
-						<span className="flex">
-							{Array.from(Array(5 - item.reviews.mobile)).map((_, index) => (
-								<span key={index}>
-									<StarEmpty width="31" height="31" />
-								</span>
-							))}
-						</span>
-					</span>
-					<span
-						aria-describedby={item.reviews.mobile_min + " star rating"}
-						className="-sm:flex hidden"
-					>
-						<span className="flex">
-							{Array.from(Array(item.reviews.mobile_min)).map((_, index) => (
-								<span key={index}>
-									<StarFilled width="31" height="31" />
-								</span>
-							))}
-						</span>
-						<span className="flex">
-							{Array.from(Array(5 - item.reviews.mobile_min)).map(
-								(_, index) => (
+							<span
+								aria-describedby={item.reviews.desktop + " star rating"}
+								className="flex"
+							>
+								{Array.from(Array(item.reviews.desktop)).map((_, index) => (
+									<span key={index}>
+										<StarFilled width="31" height="31" />
+									</span>
+								))}
+							</span>
+							<span className="flex">
+								{Array.from(Array(5 - item.reviews.desktop)).map((_, index) => (
 									<span key={index}>
 										<StarEmpty width="31" height="31" />
 									</span>
-								)
-							)}
+								))}
+							</span>
 						</span>
-					</span>
+						<span
+							aria-describedby={item.reviews.tablet + " star rating"}
+							className="-lg:flex -md:hidden hidden"
+						>
+							<span className="flex">
+								{Array.from(Array(item.reviews.tablet)).map((_, index) => (
+									<span key={index}>
+										<StarFilled width="31" height="31" />
+									</span>
+								))}
+							</span>
+							<span className="flex">
+								{Array.from(Array(5 - item.reviews.tablet)).map((_, index) => (
+									<span key={index}>
+										<StarEmpty width="31" height="31" />
+									</span>
+								))}
+							</span>
+						</span>
+						<span
+							aria-describedby={item.reviews.mobile + " star rating"}
+							className="-md:flex -sm:hidden hidden"
+						>
+							<span className="flex">
+								{Array.from(Array(item.reviews.mobile)).map((_, index) => (
+									<span key={index}>
+										<StarFilled width="31" height="31" />
+									</span>
+								))}
+							</span>
+							<span className="flex">
+								{Array.from(Array(5 - item.reviews.mobile)).map((_, index) => (
+									<span key={index}>
+										<StarEmpty width="31" height="31" />
+									</span>
+								))}
+							</span>
+						</span>
+						<span
+							aria-describedby={item.reviews.mobile_min + " star rating"}
+							className="-sm:flex hidden"
+						>
+							<span className="flex">
+								{Array.from(Array(item.reviews.mobile_min)).map((_, index) => (
+									<span key={index}>
+										<StarFilled width="31" height="31" />
+									</span>
+								))}
+							</span>
+							<span className="flex">
+								{Array.from(Array(5 - item.reviews.mobile_min)).map(
+									(_, index) => (
+										<span key={index}>
+											<StarEmpty width="31" height="31" />
+										</span>
+									)
+								)}
+							</span>
+						</span>
+					</div>
 				</div>
 				<div className="flex flex-col gap-0.5 justify-center -lg:row-start-2 -lg:row-end-3 -lg:col-start-2 -lg:col-end-3 -md:col-start-1 -md:col-end-2 -md:row-start-3 -md:row-end-4 ">
 					<p className="font-semibold flex -md:justify-center desktop-min:justify-center desktop-min:items-center">
